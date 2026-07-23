14f16816d86d0d5a70d5733f33eb4c74fd1f4027
Discordant10 <21F3001992@ds.study.iitm.ac.in>
2026-07-23 11:22:55 +0000

    Add FastAPI inference API

--- app.py
+++ app.py
@@ -1,21 +1,100 @@
+from pathlib import Path
+
+import pandas as pd
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-from inference import predict_iris
+from feast import FeatureStore
+from mlflow_utils import load_registered_model
 
-app = FastAPI()
+
+app = FastAPI(
+    title="IRIS Prediction API",
+    version="1.0.0",
+)
+
+print("Loading model...")
+model = load_registered_model()
+print("Model loaded")
+
+project_root = Path(__file__).resolve().parent
+
+feature_repo = (
+    project_root
+    / "feature_repo"
+)
+
+store = FeatureStore(
+    repo_path=str(feature_repo)
+)
+
+
+class PredictionRequest(BaseModel):
+    iris_id: int
 
 
 @app.get("/")
-def health():
-    return {"status": "running"}
+def health_check():
+    return {
+        "status": "healthy"
+    }
 
 
-@app.get("/predict/{iris_id}")
-def predict(iris_id: int):
+@app.post("/predict")
+def predict(
+    request: PredictionRequest,
+):
+    features = (
+        store.get_online_features(
+            features=[
+                "iris_features:sepal_length",
+                "iris_features:sepal_width",
+                "iris_features:petal_length",
+                "iris_features:petal_width",
+            ],
+            entity_rows=[
+                {
+                    "iris_id": request.iris_id
+                }
+            ],
+        )
+        .to_dict()
+    )
 
-    prediction = predict_iris(iris_id)
+    if (
+        len(features["sepal_length"]) == 0
+        or features["sepal_length"][0]
+        is None
+    ):
+        return {
+            "error":
+            f"No features found "
+            f"for iris_id="
+            f"{request.iris_id}"
+        }
+
+    X = pd.DataFrame(
+        {
+            "sepal_length": [
+                features["sepal_length"][0]
+            ],
+            "sepal_width": [
+                features["sepal_width"][0]
+            ],
+            "petal_length": [
+                features["petal_length"][0]
+            ],
+            "petal_width": [
+                features["petal_width"][0]
+            ],
+        }
+    )
+
+    prediction = model.predict(X)
 
     return {
-        "iris_id": iris_id,
-        "prediction": prediction,
+        "iris_id": request.iris_id,
+        "prediction": str(
+            prediction[0]
+        ),
     }
